4161c993e872ff92b4979b2c6fb0134bea2a308b
Aldo Mendoza <aldomendoza56@gmail.com>
2024-03-03 20:37:35 -0800

    adding files

---
+++ models/Post.js
@@ -0,0 +1,51 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const User = require('./User');
+
+class Post extends Model {}
+
+Post.init(
+  {
+    // Define model fields
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'post',
+  }
+);
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+module.exports = Post;
---
+++ models/User.js
@@ -0,0 +1,48 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class User extends Model {
+  // Define a method to compare the provided password with the stored hashed password
+  checkPassword(password) {
+    return bcrypt.compareSync(password, this.password);
+  }
+}
+
+User.init(
+  {
+    // Define model fields
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      set(value) {
+        const hashedPassword = bcrypt.hashSync(value, 10); // Hash the password with bcrypt
+        this.setDataValue('password', hashedPassword);
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+    name: {
+      singular: 'user',
+      plural: 'users',
+    },
+  }
+);
+
+module.exports = User;
